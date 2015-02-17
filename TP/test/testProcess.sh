acbab3637304e75e0fe8df94e55a321e27ed4825
Jonathan BAUDIN <jjbaudin@gmail.com>
2015-02-17 12:30:17 +0100

    Ajout procédure de test

--- TP/test/testProcess.sh
+++ TP/test/testProcess.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
 # Teste si les fichiers générés sont justes
+DIRECTORYSOURCE="./ressources/ProgrammeSource/*" #Dossier des fichier .l
 DIRECTORYTRUE="./ressources/AnalyseLexicale/*" #Dossier dans lequel il y a les corrections
 DIRECTORYTOTEST="./" #Dossier dans lequel il y a les fichiers générés
+
+
+for f in $DIRECTORYSOURCE
+do
+  echo "Execution du fichier : $f"
+  ../test_yylex $f
+  echo
+  echo
+done
+
+
 
 for f in $DIRECTORYTRUE
 do
